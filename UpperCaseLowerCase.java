b06254c6d3704448649355c59be1bf0704e5aba2
kazuikios <slash0juice@gmail.com>
2021-02-07 12:19:03 +0900

    modify sample

--- UpperCaseLowerCase.java
+++ UpperCaseLowerCase.java
@@ -4,28 +4,28 @@ public class UpperCaseLowerCase {
     public static void main(String[] args) {
         // 大文字→小文字変換
         String upperWord = "ABCDE";
-        System.out.println(upperWord.toLowerCase());
+        System.out.println(toLowerCaseIfNotNull(upperWord));
 
         // 小文字→大文字変換
         String lowerWord = "abcde";
-        System.out.println(lowerWord.toLowerCase());
+        System.out.println(toUpperCaseIfNotNull(lowerWord));
 
-        // nullを変換しようとするとエラー発生
+        // nullを変換しようとすると空が返却される
         String nullWord = null;
-        System.out.println(nullWord.toLowerCase());
-    }
-
-    public static String toUpperCaseIfNotNull(String str) {
-        if (str == null) {
-            return "";
-        }
-        return str.toUpperCase();
+        System.out.println(toLowerCaseIfNotNull(nullWord));
     }
 
     public static String toLowerCaseIfNotNull(String str) {
         if (str == null) {
-            return "";
+            return "空だよー";
         }
-        return str.toLowerCase();
+        return str.toLowerCase(Locale.ENGLISH);
+    }
+
+    public static String toUpperCaseIfNotNull(String str) {
+        if (str == null) {
+            return "空だよー";
+        }
+        return str.toUpperCase(Locale.ENGLISH);
     }
 }
